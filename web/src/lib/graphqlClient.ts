0c0a45f38f9d6036acff85c6ea3c29d1135e4b91
Dave <david.tomusk@gmail.com>
2026-01-10 22:27:01 +0000

    Change api location to env variable

--- web/src/lib/graphqlClient.ts
+++ web/src/lib/graphqlClient.ts
@@ -2,6 +2,5 @@ import { GraphQLClient } from "graphql-request";
 
 // Knows API location and makes requests
 export const graphqlClient = new GraphQLClient(
-    // TODO: move to env variable
-    "http://localhost:8080/query"
+    import.meta.env.VITE_API_URL
 );
