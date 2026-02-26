1038715671a8f4b1e3ce800370fc3d476138859d
Dave <david.tomusk@gmail.com>
2026-02-26 14:52:33 +0000

    Navigate to created recipe on create and add close and disappear to alert

--- web/src/pages/RecipePage.tsx
+++ web/src/pages/RecipePage.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { Alert, BackLink, PageTitle, Spinner } from "@/components";
 import { useRecipe } from "@/features/recipes";
 import { Page } from "@/layout";
@@ -7,38 +7,51 @@ import IngredientList from "@/features/recipes/components/IngredientList";
 import SectionTitle from "@/components/ui/SectionTitle";
 import Container from "@/components/layout/Container";
 import Stack from "@/components/layout/Stack";
+import { useEffect, useState } from "react";
 
 export default function RecipePage() {
     const { id } = useParams<{ id: string }>();
     const { data: recipe, isLoading, error } = useRecipe(id!);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [successMessage, setSuccessMessage] = useState<string | undefined>(location.state?.successMessage);
+
+    useEffect(() => {
+        if (successMessage) {
+            navigate(location.pathname, { replace: true, state: {} });
+        }
+    }, [successMessage, location.pathname, navigate]);
 
     return (
         <Page>
             <Container size="xl">
                 <Stack space="xl">
-                <BackLink to="/recipe" />
-                {!id && <Alert message="No recipe ID provided." />}
-                {isLoading && <Spinner />}
-                {error && <Alert message={(error as Error).message} />}
-                {recipe ? (
-                <>
-                <PageTitle text={recipe ? recipe.name : "Recipe Page"} />
-                <SharedBy userName={recipe.user} />
-                <Stack space="sm">
-                    <div className="text-center">Prep time: {recipe.prepMins} mins</div>
-                    <div className="text-center">Cook time: {recipe.cookMins} mins</div>
-                    <div className="text-center">Portions: {recipe.portions}</div>
-                </Stack>
-                <Container size="xs">
-                    <Stack space="lg">
-                        <SectionTitle text="Ingredients" />
-                        <IngredientList ingredients={recipe.ingredients} />
+                    <BackLink to="/recipe" />
+                    <Container size="sm">
+                        {!id && <Alert message="No recipe ID provided." />}
+                        {isLoading && <Spinner />}
+                        {error && <Alert message={(error as Error).message} />}
+                        {successMessage && <Alert message={successMessage} type="success" closable duration={3000} onClose={() => setSuccessMessage(undefined)} />}
+                    </Container>
+                    {recipe ? (
+                    <>
+                    <PageTitle text={recipe ? recipe.name : "Recipe Page"} />
+                    <SharedBy userName={recipe.user} />
+                    <Stack space="sm">
+                        <div className="text-center">Prep time: {recipe.prepMins} mins</div>
+                        <div className="text-center">Cook time: {recipe.cookMins} mins</div>
+                        <div className="text-center">Portions: {recipe.portions}</div>
                     </Stack>
-                </Container>
-                </>
-            ) : (
-                !isLoading && id && !error && <Alert message="Recipe not found." />
-            )}
+                    <Container size="xs">
+                        <Stack space="lg">
+                            <SectionTitle text="Ingredients" />
+                            <IngredientList ingredients={recipe.ingredients} />
+                        </Stack>
+                    </Container>
+                    </>
+                ) : (
+                    !isLoading && id && !error && <Alert message="Recipe not found." />
+                )}
                 </Stack>
             </Container>
         </Page>
