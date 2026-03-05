cf2e83cc2c5cad582bc0d647af092dbb620ea390
Dave <david.tomusk@gmail.com>
2026-03-05 17:57:36 +0000

    Update seed to seed different users each time it's called

--- api/internal/testutil/seeds/seed_users.go
+++ api/internal/testutil/seeds/seed_users.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"foodplanner/internal/db"
 	"foodplanner/internal/user"
+	"math/rand"
 
 	"github.com/google/uuid"
 )
@@ -16,15 +17,30 @@ func InsertUser(ctx context.Context, db db.DBTX, user *user.User) error {
 
 func SeedTestUser(ctx context.Context, db db.DBTX) (*user.User, error) {
 	userID := uuid.New()
+	email := randSeq(10) + "@example.com"
+	passwordHash := "test-password-hash"
+	username := randSeq(8)
+
 	testUser := user.User{
 		ID:           userID,
-		Email:        "test@example.com",
-		PasswordHash: "test-password-hash",
-		Username:     "testuser",
+		Email:        email,
+		PasswordHash: passwordHash,
+		Username:     username,
 	}
 	err := InsertUser(context.Background(), db, &testUser)
 	if err != nil {
 		return nil, err
 	}
 	return &testUser, nil
+}
+
+// TODO: move somewhere else
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }
