b5209469f2a55e64a4d576167c0175ccefd06cf0
Dave <david.tomusk@gmail.com>
2026-03-14 16:57:58 +0000

    Add create button to my recipes page

--- web/src/pages/MyRecipesPage.tsx
+++ web/src/pages/MyRecipesPage.tsx
@@ -1,4 +1,4 @@
-import { Alert, PageTitle, Spinner } from "@/components";
+import { Alert, PageTitle, Spinner, Button, Inline } from "@/components";
 import Container from "@/components/layout/Container";
 import Stack from "@/components/layout/Stack";
 import IconButton from "@/components/ui/IconButton";
@@ -6,8 +6,9 @@ import Text from "@/components/ui/Text";
 import { RecipeList, useMyRecipes } from "@/features/recipes";
 import { Page } from "@/layout";
 import { extractErrorMessage } from "@/lib/errors";
-import { Edit, Eye } from "lucide-react";
+import { Edit, Eye, Plus } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { commonStrings } from "@/lib/strings";
 
 export default function MyRecipesPage() {
     const { data, isLoading, error } = useMyRecipes();
@@ -22,6 +23,16 @@ export default function MyRecipesPage() {
             {error && <Alert message={extractErrorMessage(error)} closable />}
             {data && (
                 <>
+                    <Inline>
+                        <Stack space="sm">
+                            <Button onClick={() => navigate("/recipes/create")} 
+                            aria-label="Add new recipe" variant="primary">
+                                <Inline>
+                                <Plus /> {commonStrings.recipe.add}
+                                </Inline>
+                            </Button>
+                        </Stack>
+                    </Inline>
                     <Text>Here you can find all the recipes you've shared.</Text>
                     <RecipeList recipes={data}
                         onCardClick={()=>{}}
