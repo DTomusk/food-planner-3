d65e42616cfd011401a481679d5762eb6dfe0d6c
Dave <david.tomusk@gmail.com>
2026-03-17 13:52:27 +0000

    Configure query client

--- web/src/app/queryClient.ts
+++ web/src/app/queryClient.ts
@@ -1,4 +1,15 @@
 import { QueryClient } from "@tanstack/query-core";
 
 // Responsible for caching and managing server state
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+    defaultOptions: {
+    queries: {
+      retry: 1, 
+      retryDelay: attemptIndex => Math.min(1000 * 2 ** attemptIndex, 30000), 
+      staleTime: 1000 * 60, 
+    },
+    mutations: {
+      retry: 1,
+    },
+  },
+});
