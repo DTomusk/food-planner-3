061a72fd187ec50b0e0a55250544d9a91634de30
Dave <david.tomusk@gmail.com>
2026-03-17 16:41:08 +0000

    Define refresh token

---
+++ api/internal/auth/refresh_tokens/refresh_token.go
@@ -0,0 +1,39 @@
+package refreshtokens
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type RefreshToken struct {
+	ID uuid.UUID
+	// Actual token, do not persist in db
+	Token string
+	// Hash of the token, persist in db
+	TokenHash    string
+	IsRevoked    bool
+	ExpiresAt    int64
+	UserID       uuid.UUID
+	IPAddress    string
+	FamilyID     uuid.UUID
+	ReplacedByID *uuid.UUID
+}
+
+func newRefreshToken(token, tokenHash string, userID uuid.UUID, ipAddress string, expiresInDays int) *RefreshToken {
+	expiresAt := time.Now().Add(time.Duration(expiresInDays) * 24 * time.Hour).Unix()
+	familyID := uuid.New()
+	tokenID := uuid.New()
+
+	return &RefreshToken{
+		ID:           tokenID,
+		Token:        token,
+		TokenHash:    tokenHash,
+		UserID:       userID,
+		IPAddress:    ipAddress,
+		ExpiresAt:    expiresAt,
+		FamilyID:     familyID,
+		ReplacedByID: nil,
+		IsRevoked:    false,
+	}
+}
