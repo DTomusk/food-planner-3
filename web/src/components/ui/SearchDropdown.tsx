cd2eabd7c9b202f15e9801d491bac80436cae49a
Dave <david.tomusk@gmail.com>
2026-03-20 21:38:06 +0000

    Add searchable dropdown component with story

---
+++ web/src/components/ui/SearchDropdown.tsx
@@ -0,0 +1,70 @@
+import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions } from "@headlessui/react";
+import clsx from "clsx";
+import { CheckIcon, ChevronDown } from "lucide-react";
+import { useState } from "react";
+
+type SearchDropdownItem = {
+    label: string
+    value: string
+}
+
+type SearchDropdownProps = {
+    items: SearchDropdownItem[];
+    onSelect: (item: SearchDropdownItem | null) => void;
+    selectedItem?: SearchDropdownItem | null;
+}
+
+export default function SearchDropdown({ items, onSelect, selectedItem }: SearchDropdownProps) {
+    const [query, setQuery] = useState("");
+
+    const defaultFilter = (query: string) => 
+        items.filter((item) => item.label.toLowerCase().includes(query.toLowerCase()));
+
+    const filteredItems = query === "" ? items : defaultFilter(query);
+
+    return (
+         <div className="mx-auto h-screen w-52 pt-20">
+            <Combobox value={selectedItem} onChange={onSelect} onClose={() => setQuery("")}>
+                <div className="relative">
+                    <ComboboxInput 
+                        displayValue={(item?: SearchDropdownItem) => item?.label ?? ""}
+                        onChange={(event) => setQuery(event.target.value)}
+                        className={clsx(
+                            "w-full border border-gray-300 bg-white py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:border-blue-500 focus:outline-none focus-ring",
+                        )}
+                    />
+                    <ComboboxButton className="group absolute inset-y-0 right-0 px-2.5">
+                        <ChevronDown className="size-4 fill-white/60 group-data-hover:fill-white" />
+                    </ComboboxButton>
+                </div>
+
+                <ComboboxOptions
+                    anchor="bottom"
+                    transition
+                    className={clsx(
+                        'w-(--input-width) border border-black bg-white p-1 [--anchor-gap:--spacing(1)] empty:invisible',
+                        'transition duration-100 ease-in data-leave:data-closed:opacity-0'
+                    )}    
+                >
+                    {filteredItems.map((item) => (
+                        <ComboboxOption 
+                        key={item.value} 
+                        value={item}
+                        className={({ active }) =>
+                            clsx(
+                            "flex cursor-pointer items-center gap-2 rounded-sm px-3 py-1.5 select-none",
+                            active ? "bg-primary-200 text-black" : "text-gray-900",
+                            "hover:bg-primary-100"
+                            )
+                        }
+                        >
+                            <CheckIcon className="invisible size-4 fill-white group-data-selected:visible" />
+                            {item.label}
+                        </ComboboxOption>
+                    ))}
+                </ComboboxOptions>
+            </Combobox>
+            
+         </div>
+    );
+}
