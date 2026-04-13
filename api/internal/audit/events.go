50273e8d81aaffdc1566dc5ee9051ee818582a9b
Dave <david.tomusk@gmail.com>
2026-04-13 22:00:21 +0100

    Implement repo method

--- api/internal/audit/events.go
+++ api/internal/audit/events.go
@@ -1,7 +1,23 @@
 package audit
 
-func NewUserSignupEvent() *AuditEntry {
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func NewUserSignupEvent(correlationID uuid.UUID, userID uuid.UUID, newState json.RawMessage) *AuditEntry {
 	return &AuditEntry{
-		Action: ActionUserSignup,
+		ID:            uuid.New(),
+		CorrelationID: correlationID,
+		ActorID:       &userID,
+		ResourceType:  ResourceTypeUser,
+		ResourceID:    &userID,
+		Action:        ActionUserSignup,
+		Result:        ResultSuccess,
+		CreatedAt:     time.Now(),
+		NewState:      newState,
+		Context:       nil,
 	}
 }
--- api/internal/audit/repo.go
+++ api/internal/audit/repo.go
@@ -7,10 +7,45 @@ import (
 
 type repo struct{}
 
+const insertAuditQuery = `
+INSERT INTO audits (
+	id,
+	correlation_id,
+	actor_id,
+	resource_type,
+	resource_id,
+	action,
+	created_at,
+	result,
+	old_state,
+	new_state,
+	reason,
+	context
+)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+`
+
 func NewRepo() *repo {
 	return &repo{}
 }
 
-func (r *repo) Save(ctx context.Context, db db.DBTX, entry *AuditEntry) error {
-	return nil
+func (r *repo) Save(ctx context.Context, database db.DBTX, entry *AuditEntry) error {
+	_, err := database.ExecContext(
+		ctx,
+		insertAuditQuery,
+		entry.ID,
+		entry.CorrelationID,
+		entry.ActorID,
+		entry.ResourceType,
+		entry.ResourceID,
+		entry.Action,
+		entry.CreatedAt,
+		entry.Result,
+		entry.OldState,
+		entry.NewState,
+		entry.Reason,
+		entry.Context,
+	)
+
+	return err
 }
