cd65a5f878fd41e1ecd8114700a27f89cc59e511
Dave <david.tomusk@gmail.com>
2026-04-27 22:39:41 +0100

    Fix migration to be backwards compatible

--- api/migrations/0031_alter_recipes_add_published_at.up.sql
+++ api/migrations/0031_alter_recipes_add_published_at.up.sql
@@ -1,6 +1,9 @@
 ALTER TABLE recipe_containers
 ADD COLUMN published_at timestamptz;
 
+UPDATE recipe_containers rc
+SET published_at = rc.created_at;
+
 -- Make fetching published not deleted recipes more efficient
 CREATE INDEX idx_recipe_containers_public_created_at_id
 ON recipe_containers (created_at DESC, id DESC)
@@ -8,6 +11,9 @@ WHERE deleted_on IS NULL AND published_at IS NOT NULL;
 
 ALTER TABLE recipe_versions
 ADD COLUMN published_at timestamptz;
+
+UPDATE recipe_versions rv
+SET published_at = rv.created_at;
 
 -- do not allow multiple drafts per recipe
 CREATE UNIQUE INDEX ux_recipe_versions_one_draft_per_recipe
